a5c096e6f604e323955a30de6367da5647d27e19
Dmitry Makarenko <kryksyh@gmail.com>
2026-06-16 17:50:13 +0300

    MNX: mnxdom, mnx_w3c, nlohmann_json, json_schema_validator

---
+++ mnx_w3c/mnx_w3c.cmake
@@ -0,0 +1,3 @@
+# No releases for mnx, use sha as a version
+set(DEP_VERSION d513cf7)
+set(DEP_KIND source)
---
+++ nlohmann_json/nlohmann_json.cmake
@@ -0,0 +1,4 @@
+set(DEP_VERSION 3.12.0)
+
+# Header-only library
+set(DEP_SYSTEM_HEADER nlohmann/json.hpp)
